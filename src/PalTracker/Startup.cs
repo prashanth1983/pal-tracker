e6fe3ae0c3900278538c61c907685bb222a52079
Prashanth <acharprashanth@gmail.com>
2019-09-05 11:27:41 +0530

    integrating my sql database

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 
 namespace PalTracker
 {
@@ -35,7 +36,9 @@ namespace PalTracker
                Configuration.GetValue<string>("CF_INSTANCE_INDEX", "CF_INSTANCE_INDEX_NOT_CONFIGURED"),
                Configuration.GetValue<string>("CF_INSTANCE_ADDR", "CF_INSTANCE_ADDR_NOT_CONFIGURED")
            ));
-           services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+           //services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
+           services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
+           services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- test/PalTrackerTests/DbTestSupport.cs
+++ test/PalTrackerTests/DbTestSupport.cs
@@ -6,8 +6,8 @@ namespace PalTrackerTests
 {
     public static class DbTestSupport
     {
-        public const string TestDbConnectionString = "server=127.0.0.1;uid=tracker_dotnet;pwd=password;database=tracker_dotnet_test";
-
+       // public const string TestDbConnectionString = "server=127.0.0.1;uid=tracker_dotnet;pwd=password;database=tracker_dotnet_test";
+        public const string TestDbConnectionString = "Server=localhost;Database=tracker_dotnet_test;Uid=tracker_dotnet;Pwd=password";
         public static IList<IDictionary<string, object>> ExecuteSql(string sql)
         {
             var result = new List<IDictionary<string, object>>();
